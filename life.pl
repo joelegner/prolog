dd0282812c6d083cff08512e3c74526518d0199c
Joe Legner <joelegner@gmail.com>
2025-04-22 00:21:22 -0400

    Adds to life.pl

--- life.pl
+++ life.pl
@@ -3,6 +3,11 @@ julie.
 joey.
 tommy.
 
+bob.
+terry.
+elaine.
+margo.
+
 aristotle.
 brando.
 cher.
@@ -13,6 +18,10 @@ person(julie).
 person(joey).
 person(tommy).
 person(cher).
+person(bob).
+person(terry).
+person(elaine).
+person(margo).
 
 living(joe).
 living(julie).
@@ -20,16 +29,33 @@ living(joey).
 living(tommy).
 living(cher).
 living(dylan).
+living(bob).
+living(terry).
 
-father(joe, joey).
-father(joe, tommy).
-mother(julie, joey).
-mother(julie, tommy).
+female(julie).
+female(elaine).
+female(margo).
+female(cher).
+
 male(joe).
 male(joey).
 male(tommy).
-female(julie).
-female(cher).
+male(bob).
+male(terry).
+male(dylan).
+
+father(joe, joey).
+father(joe, tommy).
+father(terry, julie).
+father(bob, joe).
+mother(julie, joey).
+mother(julie, tommy).
+mother(elaine, joe).
+mother(margo, julie).
+
+grandparent(GP, C) :- parent(GP, P), parent(P, C).
+grandmother(GP, C) :- grandparent(GP, C), female(GP).
+grandfather(GP, C) :- grandparent(GP, C), male(GP).
 
 husband(joe, julie).
 wife(julie, joe).
