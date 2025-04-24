0a9898681850cbc74fb12e0e4a015d916cfeed76
Joe Legner <joelegner@gmail.com>
2025-04-24 13:51:08 -0400

    Adds code to eliminate repeated instructions

--- piano_setup.pl
+++ piano_setup.pl
@@ -1,3 +1,5 @@
+:- dynamic done/1.
+
 % There are some real world objects 
 % Venue
 floor.              % Everything sits on the floor.
@@ -67,12 +69,44 @@ source(microphone, signal).
 % X = load(piano_shell) ;
 % false.
 
-setup(piano_shell) :- writeln("Set up piano shell").
-setup(drum_throne) :- writeln("Set up drum throne").
-setup(microphone_stand) :- setup(piano_shell), writeln("Set up microphone stand.").
-setup(keyboard) :- setup(piano_shell), setup(drum_throne), writeln("Set up keyboard").
-setup(microphone) :- setup(microphone_stand), writeln("Set up microphone.").
+setup(piano_shell) :-
+    \+ done(piano_shell),
+    writeln("Set up piano shell"),
+    assertz(done(piano_shell)).
+setup(piano_shell) :- done(piano_shell).
 
+setup(drum_throne) :-
+    \+ done(drum_throne),
+    writeln("Set up drum throne"),
+    assertz(done(drum_throne)).
+setup(drum_throne) :- done(drum_throne).
+
+setup(microphone_stand) :-
+    setup(piano_shell),
+    \+ done(microphone_stand),
+    writeln("Set up microphone stand."),
+    assertz(done(microphone_stand)).
+setup(microphone_stand) :- done(microphone_stand).
+
+setup(keyboard) :-
+    setup(piano_shell),
+    setup(drum_throne),
+    \+ done(keyboard),
+    writeln("Set up keyboard"),
+    assertz(done(keyboard)).
+setup(keyboard) :- done(keyboard).
+
+setup(microphone) :-
+    setup(microphone_stand),
+    \+ done(microphone),
+    writeln("Set up microphone."),
+    assertz(done(microphone)).
+setup(microphone) :- done(microphone).
+
+setup(microphone_cable) :-
+    setup(microphone),
+    \+ done(microphone_cable),
+    writeln("Hook up microphone cable.").
 
 % ready_to_play/0 is the over-arching goal. It is the starting goal for when you
 % use swipl at the command line. The usage works like this:
@@ -84,9 +118,11 @@ setup(microphone) :- setup(microphone_stand), writeln("Set up microphone.").
 % -g ready_to_play: Invokes this goal to get things started.
 % -t halt: Stops the interpreter after the goal is met. Remove to interact.
 ready_to_play :-
+    retractall(done(_)),
     setup(piano_shell),
     setup(drum_throne),
     setup(keyboard),
     setup(microphone),
     setup(microphone_stand),
+    setup(microphone_cable),
     writeln("Ready to play!").
