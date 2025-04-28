2d33eaa62b6ffe0e0e02f29fabc061b6f441674c
Joe Legner <joelegner@gmail.com>
2025-04-28 15:13:57 -0400

    Refactors system.pl

--- system.pl
+++ system.pl
@@ -1,15 +1,28 @@
-system(Sys) :- has_parts(Sys, _), has_purpose(Sys, _).
+system(flashlight, illuminate_area, [head, body]).
+system(body, contain_battery, [cylinder, battery]).
+system(head, make_light, [lens, bulb, housing]).
+system(bulb, convert_elec_to_light, [enclosure, filament, connections]).
+system(battery, supply_voltage, []).
 
-has_parts(capsule, [electrical_leads]).
-has_parts(microphone, [body, windscreen, capsule]).
-has_purpose(capsule, transduce_sound_to_electricity).
-has_purpose(microphone, transduce_sound_to_electricity).
+% A subsystem is a system that is part of a larger system.
+% X is the system being unified.
+% P is a list of parts.
+subsystem(X) :- 
+    system(X, _, _),
+    system(_, _, P),
+    member(X, P).
 
-% ?- system(capsule).
-% true.
+not_empty_list([_|_]).
 
-% ?- system(body).
-% false.
+% A system with parts is an assembly
+% A system with a non-empty parts list is an assembly.
+% P is a list of parts
+assembly(X) :-
+    system(X, _, P),
+    not_empty_list(P).
 
-% ?- system(microphone).
-% true.
+% A system with zero parts is an assembly.
+% An assembly has an empty parts list.
+% P is a list of parts.
+unit(X) :-
+    system(X, _, []).
