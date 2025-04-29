1351e2295771cfbf65c5649947cf6a5242c71742
Joe Legner <joelegner@gmail.com>
2025-04-29 19:40:41 -0400

    Refactors completely axiomatic_design.pl

--- axiomatic_design.pl
+++ axiomatic_design.pl
@@ -1,58 +1,23 @@
-% Your facts
-provides(dp(desk), fr(provides_desk_functionality)).
-provides(dp(desktop), fr(provide_working_surface)).
-provides(dp(leg_assembly), fr(support_working_surface)).
+% ad(FR, DP, [Needs_met]).
+ad(make_dueling_more_fun, dueler_mvp_project, [have_fun, edit_database, find_songs, play_songs, sing_songs]).
+ad(install_app, app_store_entry, [download, install, play_with]).
+ad(run_app, dueler_mvp_app, [use_app]).
 
-provides(dp(wood_panel), fr(provide_writing_surface)).
-provides(dp(drilled_holes), fr(attach_to_legs)).
-provides(dp(edge_treatment), fr(provide_comfort)).
+ad(develop_app, development_system, [use_app]).
+ad(deploy_app, app_store_process, [download, install, play_with]).
+ad(host_app_for_download, app_store, [discover_app, download, install, play_with]).
 
-provides(dp(wood), fr(act_as_panel)).
-provides(dp(wood_finish_system), fr(protect_wood)).
+ad(interface_with_user, user_interface_code, [use_app, play_with]).
+ad(manage_data, data_model_code, [find_songs, edit_database]).
 
-parts(desk, [desktop, leg_assembly]).
-parts(desktop, [wood_panel, drilled_holes, edge_treatment]).
-parts(wood_panel, [wood, wood_finish_system]).
+children(make_dueling_more_fun, [install_app, run_app]).
+children(install_app, [develop_app, deploy_app, host_app_for_download]).
+children(use_app, [interface_with_user, manage_data]).
 
-% Recursive hierarchy printing
-print_hierarchy :-
-    print_dp(dp(desk), 0).
+need(X) :-
+    ad(_, _, Needs), 
+    member(X, Needs).
 
-print_dp(DP, Indent) :-
-    provides(DP, FR),
-    DP = dp(DPName),
-    FR = fr(FRName),
-    tab(Indent), format("~w provides ~w~n", [DPName, FRName]),
-    ( parts(DPName, SubParts) ->
-        NextIndent is Indent + 2,
-        print_subparts(SubParts, NextIndent)
-    ; true ).
-
-print_subparts([], _).
-print_subparts([SubPart|Rest], Indent) :-
-    print_dp(dp(SubPart), Indent),
-    print_subparts(Rest, Indent).
-
-% ?- print_hierarchy.
-% desk provides provide_desk_functionality
-%   desktop provides provide_working_surface
-%     wood_panel provides provide_writing_surface
-%       wood provides act_as_panel
-%       wood_finish_system provides protect_wood
-%     drilled_holes provides attach_to_legs
-%     edge_treatment provides provide_comfort
-%   leg_assembly provides support_working_surface
-% true.
-
-% Let's define the physicality of the system
-% A design parameter is a physical variable.
-physical(X) :- provides(dp(X), _).
-    
-all_physical_objects(List) :-
-    findall(X, physical(X), List).
-
-% Get functional requirements from facts
-functional_requirement(X) :- provides(_, fr(X)).
-
-all_functional_requirements(List) :-
-    findall(X, functional_requirement(X), List).
+satisfied_by(Need, FR) :-
+    ad(FR, _, Needs),
+    member(Need, Needs).
