b32d67ae81112de2eefc90b0c8a275cbe89cc56e
Joe Legner <joelegner@gmail.com>
2025-04-30 08:15:46 -0400

    Refactors philosophy

--- philosophy.pl
+++ philosophy.pl
@@ -1,6 +1,21 @@
-has_quality(writing, thinking).
-has_quality(talking, thinking).
+% Good is a quality independent of conditions.
+goodness.
 
-% ?- has_quality(X, thinking).
-% X = writing ;
-% X = talking.
+% Identify things as having goodness or not
+has(health, goodness).
+has(love, goodness).
+has(care, goodness).
+has(caring, goodness).
+has(concern, goodness).
+has(pleasure, goodness).
+
+does_not_have(pain, goodness).
+does_not_have(anxiety, goodness).
+does_not_have(addiction, goodness).
+does_not_have(harm, goodness).
+does_not_have(hate, goodness).
+
+is_a(self_harm, harm).
+
+all_good(Goods) :-
+     findall(Good, has(Good, goodness), Goods).
