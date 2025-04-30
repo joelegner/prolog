abb2d4b28ff2ab9c464a4500a1729c2596be1ea3
Joe Legner <joelegner@gmail.com>
2025-04-30 08:34:48 -0400

    Adds mindful/1 predicate

--- buddhism.pl
+++ buddhism.pl
@@ -13,5 +13,13 @@ stops(P, dukkha) :- stops(P, clinging).
 % The Fourth Noble Truth:
 stops(P, clinging) :- follows(P, eightfold_noble_path).
 
+% Mindfulness
+mindful(Person) :-
+    P = Person, % New thign I tried to improve readability
+    aware(P, present_moment),
+    balancing(P, the_mind),
+    guarding(P, six_sense_doors),
+    remembering(P, [buddha, dharma, sangha]).
+
 main :-
     writeln("buddhism.pl").
