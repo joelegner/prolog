caf86546849fb935bea07ff373758b4873f6aeab
Joe Legner <joelegner@gmail.com>
2025-05-01 12:03:14 -0400

    Adds remove_duplicates/2 with tests

--- lists.pl
+++ lists.pl
@@ -19,29 +19,15 @@ print_list([H|T]) :-
 append_lists(List1, List2, Result) :-
     append(List1, List2, Result).
 
+% Remove duplicates from a list
+remove_duplicates([], []).
+remove_duplicates([First|Rest], [First|New_Rest]) :-
+    exclude(==(First), Rest, Filtered),
+    remove_duplicates(Filtered, New_Rest).
+
 % TEST CODE FOR LISTS
 :- begin_tests(list_utils).
 
-% Declare a list
-my_list([apple, banana, cherry]).
-
-% Get the first item in a list
-first_item([First|_], First).
-
-% Get the rest of the list
-rest_of_list([_|Rest], Rest).
-
-% Iterate to print all items in a list
-print_list([]).
-print_list([H|T]) :-
-    writeln(H),
-    print_list(T).
-
-% Append two lists
-append_lists(List1, List2, Result) :-
-    append(List1, List2, Result).
-
-% Tests
 test(first_item) :-
     my_list(L),
     first_item(L, F),
@@ -59,5 +45,22 @@ test(print_list, true) :-
 test(append_lists) :-
     append_lists([1, 2], [3, 4], R),
     assertion(R == [1, 2, 3, 4]).
+
+% Tests for remove_duplicates
+test(remove_duplicates_no_dupes) :-
+    remove_duplicates([a, b, c], R),
+    assertion(R == [a, b, c]).
+
+test(remove_duplicates_with_dupes) :-
+    remove_duplicates([a, b, a, c, b], R),
+    assertion(R == [a, b, c]). % Order of kept elements is reversed due to logic
+
+test(remove_duplicates_all_same) :-
+    remove_duplicates([x, x, x, x], R),
+    assertion(R == [x]).
+
+test(remove_duplicates_empty) :-
+    remove_duplicates([], R),
+    assertion(R == []).
 
 :- end_tests(list_utils).
