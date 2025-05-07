9823816676a55ba383cb039b67ff5c987cd7731f
Joe Legner <joelegner@gmail.com>
2025-05-07 08:02:23 -0400

    Makes accounting.pl based on pennies with format_dollars/2

--- accounting.pl
+++ accounting.pl
@@ -1,10 +1,9 @@
 % This is an accounting proof of concept.
 % Here is the structure of the data. It was adapted from a (much longer) spreadsheet.
-show(date(2025, 04, 20), venue(escape), pay(base(562.5), tips(cash(88), venmo(0)))).
-show(date(2025, 04, 22), venue(escape), pay(base(562.5), tips(cash(93), venmo(0)))).
-show(date(2025, 04, 24), venue(escape), pay(base(562.5), tips(cash(97), venmo(0)))).
-show(date(2025, 04, 26), venue(escape), pay(base(562.5), tips(cash(60), venmo(0)))).
-% show(date(2025, 04, 27), venue(escape), pay(base(562.5), tips(cash(123), venmo(0)))).
+show(date(2025, 04, 20), venue(escape), pay(base(56250), tips(cash(8800), venmo(0)))).
+show(date(2025, 04, 22), venue(escape), pay(base(56250), tips(cash(9300), venmo(0)))).
+show(date(2025, 04, 24), venue(escape), pay(base(56250), tips(cash(9700), venmo(0)))).
+show(date(2025, 04, 26), venue(escape), pay(base(56250), tips(cash(6000), venmo(0)))).
 
 % Rule to extract cash tips
 cash_tip(Tip) :-
@@ -42,6 +41,11 @@ average_cash_tips(Average) :-
 format_date(date(Year, Month, Day), Formatted) :-
     format(atom(Formatted), '~w/~w/~w', [Month, Day, Year]).
 
+format_dollars(Cents, Formatted) :-
+    integer(Cents),
+    Dollars is 1.0*Cents/100.0,
+    format(atom(Formatted), '$~2f', [Dollars]).
+
 % Print the full report directly
 print_cash_tips_report :-
     number_of_shows(Count),
@@ -50,8 +54,10 @@ print_cash_tips_report :-
     average_cash_tips(Average),
     format_date(Start, StartStr),
     format_date(End, EndStr),
-    format('For ~w shows from ~w to ~w, cash tips totaled $~2f for an average of $~2f per show.~n',
-           [Count, StartStr, EndStr, Total, Average]).
+    format_dollars(Total, TotalStr),
+    format_dollars(Average, AverageStr),
+    format('For ~w shows from ~w to ~w, cash tips totaled ~w for an average of ~w per show.~n',
+           [Count, StartStr, EndStr, TotalStr, AverageStr]).
 
 % And it worked!
 %
