ab50f49b1a1eaa1820493b57c8ef214f9d1c4213
Joe Legner <joelegner@gmail.com>
2025-05-13 09:34:21 -0400

    Adds comments to label_files.sh

--- label_files.sh
+++ label_files.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+#
+# ChatGPT wrote this at my prompting on May 13, 2025.
+# Here is the prompt:
+#
+#I have a bunch of Prolog files with .pl suffix. I want the top of each file to be the
+# filename in a comment. % accounting.pl
+#
+# Cycle through .pl files and add first comment
+#
 for file in *.pl; do
     first_line=$(head -n 1 "$file")
     filename_comment="% $file"
