204f72a61855a6471c5bddc951422e0d0dbd1f82
Joe Legner <joelegner@gmail.com>
2025-05-13 09:35:31 -0400

    Adds comments to label_files.sh

--- label_files.sh
+++ label_files.sh
@@ -6,8 +6,17 @@
 #I have a bunch of Prolog files with .pl suffix. I want the top of each file to be the
 # filename in a comment. % accounting.pl
 #
-# Cycle through .pl files and add first comment
+# Exmaple usage from May 13, 2025:
 #
+# % ./label_files.sh 
+# Updating accounting.pl
+# Updating act.pl
+# Updating axiology.pl
+# Skipping axiomatic_design.pl (already has filename comment)
+# Skipping basics.pl (already has filename comment)
+# ...
+#
+# Cycle through .pl files and add first comment:
 for file in *.pl; do
     first_line=$(head -n 1 "$file")
     filename_comment="% $file"
