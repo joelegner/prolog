b8a7e0252d7c788a34391a511c57534b4090c5e1
Joe Legner <joelegner@gmail.com>
2025-05-14 08:15:09 -0400

    Reorders predicates in top-down manner

--- project_ethernet.pl
+++ project_ethernet.pl
@@ -1,29 +1,61 @@
 % project_ethernet.pl
+% This demonstrated planning a project using logic.
 
-% # Things
+% It is a top-down design. 
+%
+% LEVEL 0 =====================================================================
+complete(Project) :-
+    project(Project),
+    started(Project),
+    delivers(Project, Cable),
+    working(Cable),
+    tools_put_away(Project),
+    write("END project: "),
+    writeln(Project).
+
+% LEVEL 1 =====================================================================
 project(tommy_ethernet).
 delivers(tommy_ethernet, ethernet_cable).
 cable(ethernet_cable).
+
+started(Project) :-
+    project(Project),
+    write("START project: "),
+    writeln(Project).
 
 specified(Cable) :-
     cable(Cable),
     length_known(Cable),
     writeln("Determine cable type.").
 
-length_known(Cable) :-
-    cable(Cable),
-    writeln("Determine cable length in feet.").
-
 working(Cable) :-
     cable(Cable),
     installed(Cable),
     tested(Cable).
+
+tools_put_away(Project) :-
+    project(Project),
+    ladder_taken_down(Project),
+    writeln("Put tools away.").
+
+% LEVEL 2 =====================================================================
+length_known(Cable) :-
+    cable(Cable),
+    writeln("Determine cable length in feet.").
 
 installed(Cable) :-
     cable(Cable),
     routed(Cable),
     terminated(Cable).
 
+tested(Cable) :-
+    cable(Cable),
+    writeln("Test network connection.").  
+
+ladder_taken_down(_) :-
+    writeln("Take down ladder.").
+
+% LEVEL 3 =====================================================================
 routed(Cable) :-
     % nl,
     % writeln("Route Cable:"),
@@ -31,39 +63,16 @@ routed(Cable) :-
     purchased(Cable),
     run_in_attic(Cable).
 
+terminated(Cable) :-
+    cable(Cable),
+    writeln("Terminate cable at router."),
+    writeln("Terminate cable at computer."). 
+
+% LEVEL 4 =====================================================================
 purchased(Cable) :-
     cable(Cable),
     specified(Cable),
     writeln("Buy network cable at Lowes or Home Depot.").
-    
-tool(Project, Tool) :-
-    project(Project),
-    write("Get tool: "),
-    writeln(Tool).
-
-tools(Project) :-
-    tool(Project, ladder),
-    tool(Project, lighting),
-    tool(Project, drill),
-    tool(Project, spade_bit),
-    tool(Project, saw),
-    tool(Project, screwdriver),
-    tool(Project, vacuum).    
-
-hole_cut_in_wall(_) :-
-    writeln("Cut hole in wall for cable.").
-
-hole_drilled_in_plate(_) :-
-    writeln("Drill hole in top plate of wall for cable.").
-
-run_prepared(Cable) :-
-    cable(Cable),
-    hole_cut_in_wall(Project),
-    hole_drilled_in_plate(Project).
-
-cable_pulled(Cable) :-
-    run_prepared(Cable),
-    writeln("Pull cable between endpoints.").
 
 run_in_attic(Cable) :-
     cable(Cable),
@@ -73,36 +82,37 @@ run_in_attic(Cable) :-
     cable_pulled(Cable),
     writeln("Run cable through attic").
 
+% LEVEL 5 =====================================================================
+tools(Project) :-
+    tool(Project, ladder),
+    tool(Project, lighting),
+    tool(Project, drill),
+    tool(Project, spade_bit),
+    tool(Project, saw),
+    tool(Project, screwdriver),
+    tool(Project, vacuum).    
+
 ladder_set_up(_) :-
     writeln("Set up ladder.").
 
-ladder_taken_down(_) :-
-    writeln("Take down ladder.").
+cable_pulled(Cable) :-
+    run_prepared(Cable),
+    writeln("Pull cable between endpoints.").
 
-terminated(Cable) :-
+% LEVEL 6 =====================================================================
+tool(Project, Tool) :-
+    project(Project),
+    write("Get tool: "),
+    writeln(Tool).
+
+run_prepared(Cable) :-
     cable(Cable),
-    writeln("Terminate cable at router."),
-    writeln("Terminate cable at computer.").
+    hole_cut_in_wall(Project),
+    hole_drilled_in_plate(Project).
 
-tested(Cable) :-
-    cable(Cable),
-    writeln("Test network connection.").   
+% LEVEL 7 =====================================================================
+hole_cut_in_wall(_) :-
+    writeln("Cut hole in wall for cable.").
 
-tools_put_away(Project) :-
-    project(Project),
-    ladder_taken_down(Project),
-    writeln("Put tools away.").
-
-started(Project) :-
-    project(Project),
-    write("START project: "),
-    writeln(Project).
-
-complete(Project) :-
-    project(Project),
-    started(Project),
-    delivers(Project, Cable),
-    working(Cable),
-    tools_put_away(Project),
-    write("END project: "),
-    writeln(Project).
+hole_drilled_in_plate(_) :-
+    writeln("Drill hole in top plate of wall for cable.").
