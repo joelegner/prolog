362d47eef8497a3db55ac1d0df8c4527c6fd63ea
Joe Legner <joelegner@gmail.com>
2025-05-29 14:00:38 -0400

    Adds FR3 Level 2

--- decomposition.pl
+++ decomposition.pl
@@ -2,17 +2,21 @@
 % Level 0
 fr(fr0, 'Kill it at Fluffernutters 2025').
 
-% Level 1
+% FR0 Level 1
 fr(fr1, 'Travel there and back').
 fr(fr2, 'Produce the shows').
 fr(fr3, 'Perform shows').
 
-% Level 2
+% FR1 Level 2
 fr(fr11, 'Prepare to travel').
 fr(fr12, 'Travel to Fluffernutters').
 fr(fr13, 'Travel back home').
 
-% Level 3
+% FR3 Level 2
+fr(fr31, 'Produce sound and light').
+fr(fr32, 'Play music and sing').
+
+% FR11 Level 3
 fr(fr111, 'Document what is needed').
 fr(fr112, 'Make sure everything gets packed').
 fr(fr113, 'Contain and carry things').
@@ -21,17 +25,21 @@ fr(fr113, 'Contain and carry things').
 % Level 0
 dp(dp0, 'Fluffernutters 2025 Project').
 
-% Level 1
+% DP0 Level 1
 dp(dp1, 'Travel Arrangements').
 dp(dp2, 'Production System').
 dp(dp3, 'Stage Setup').
 
-% Level 2
+% DP1 Level 2
 dp(dp11, 'Packing System').
 dp(dp12, 'Travel Day 1').
 dp(dp13, 'Travel Day 2').
 
-% Level 3
+% DP3 Level 2
+dp(dp31, 'Sound system').
+dp(dp32, 'The Wonder Twins').
+
+% DP11 Level 3
 dp(dp111, 'Apple Reminders Fluffernutters 2025 note').
 dp(dp112, 'Packing checklist instance').
 dp(dp113, 'Luggage and travel equipment and supplies').
@@ -44,18 +52,30 @@ link(dp0, fr2).
 link(fr2, dp2).
 link(dp0, fr3).
 link(fr3, dp3).
+
+% DP1 Level 2
 link(dp1, fr11).
 link(dp1, fr12).
 link(dp1, fr13).
 link(fr11, dp11).
 link(fr12, dp12).
 link(fr13, dp13).
+
+% DP11 Level 3
 link(dp11, fr111).
 link(dp11, fr112).
 link(dp11, fr113).
 link(fr111, dp111).
 link(fr112, dp112).
 link(fr113, dp113).
+
+% DP2 Level 2
+
+% DP3 Level 2
+link(dp3, fr31).
+link(dp3, fr32).
+link(fr31, dp31).
+link(fr32, dp32).
 
 % Wrapping utility
 wrap_text(Text, Width, Wrapped) :-
