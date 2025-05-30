e143073b2aa996fab4d955da67d3fcf03d9aac4d
Joe Legner <joelegner@gmail.com>
2025-05-30 12:59:57 -0400

    Adds a comment

--- main.pl
+++ main.pl
@@ -1,5 +1,6 @@
 % main.pl
 % Example of a launcher file to compile to executable design using this command:
 % swipl -o design -c main.pl
+% make main works too
 :- use_module(design).
 :- initialization(run, main).
