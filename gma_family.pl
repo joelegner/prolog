369ff41160823a66ee8725ce16c8b380ed9ed951
Joe Legner <joelegner@gmail.com>
2025-06-04 10:03:11 -0400

    Adds option to sell both

--- gma_family.pl
+++ gma_family.pl
@@ -16,11 +16,20 @@ tommy_home(apartment).
 sold(walsingham).
 sold(ottawa).
 sold(neither).
+sold(both).
 
 % --- Constraints ---
 eliminates_value(sold(Home), joe_julie_home(Home)).
 eliminates_value(sold(Home), joey_home(Home)).
 eliminates_value(sold(Home), tommy_home(Home)).
+
+% Selling both houses elimiates them as anyone's home
+eliminates_value(sold(both), joe_julie_home(walsingham)).
+eliminates_value(sold(both), joe_julie_home(ottawa)).
+eliminates_value(sold(both), tommy_home(walsingham)).
+eliminates_value(sold(both), tommy_home(ottawa)).
+eliminates_value(sold(both), joey_home(walsingham)).
+eliminates_value(sold(both), joey_home(ottawa)).
 
 % --- Convenience wrapper for the vehicle problem ---
 valid_family_config(JOE_JULIE_HOME, JOEY_HOME, TOMMY_HOME, SOLD) :-
