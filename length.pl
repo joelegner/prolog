73af7233738c3b8090b8fb17c94fdb812742a302
Joe Legner <joelegner@gmail.com>
2025-06-07 16:22:33 -0400

    Adds default and several test cases

--- length.pl
+++ length.pl
@@ -5,5 +5,13 @@
 write_length(feet(N), Digits) :-
     format('~*f ft~n', [Digits, N]).
 
+% Default case: use 3 digits.
+write_length(feet(N)) :- 
+    write_length(feet(N), 3).
+
 run :-
-    write_length(feet(6), 3).
+    write_length(feet(6), 0),
+    write_length(feet(6), 1),
+    write_length(feet(6), 2),
+    write_length(feet(6)),
+    write_length(feet(6), 4).
