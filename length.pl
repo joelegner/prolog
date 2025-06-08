3611fb6bf5f445e78c652a558efa0bca0cea2128
Joe Legner <joelegner@gmail.com>
2025-06-08 08:06:01 -0400

    Renames write to format

--- length.pl
+++ length.pl
@@ -1,33 +1,33 @@
 % length.pl
 % Length reporting predicates
 :- module(length, [
-    write_length/1,
-    write_length/2
+    format_length/1,
+    format_length/2
 ]).
 
 % Imperial units
-write_length(feet(N), Digits) :-
+format_length(feet(N), Digits) :-
     format('~*f ft~n', [Digits, N]).
-write_length(inches(N), Digits) :-
+format_length(inches(N), Digits) :-
     format('~*f in~n', [Digits, N]).
-write_length(miles(N), Digits) :-
+format_length(miles(N), Digits) :-
     format('~*f mi~n', [Digits, N]).
 
 % SI units
-write_length(millimeters(N), Digits) :-
+format_length(millimeters(N), Digits) :-
     format('~*f mm~n', [Digits, N]).
-write_length(centimeters(N), Digits) :-
+format_length(centimeters(N), Digits) :-
     format('~*f cm~n', [Digits, N]).
-write_length(meters(N), Digits) :-
+format_length(meters(N), Digits) :-
     format('~*f m~n', [Digits, N]).
-write_length(kilometers(N), Digits) :-
+format_length(kilometers(N), Digits) :-
     format('~*f km~n', [Digits, N]).
 
 % Default to 3 digits
-write_length(feet(N))        :- write_length(feet(N), 3).
-write_length(inches(N))      :- write_length(inches(N), 3).
-write_length(miles(N))       :- write_length(miles(N), 3).
-write_length(millimeters(N)) :- write_length(millimeters(N), 3).
-write_length(centimeters(N)) :- write_length(centimeters(N), 3).
-write_length(meters(N))      :- write_length(meters(N), 3).
-write_length(kilometers(N))  :- write_length(kilometers(N), 3).
+format_length(feet(N))        :- format_length(feet(N), 3).
+format_length(inches(N))      :- format_length(inches(N), 3).
+format_length(miles(N))       :- format_length(miles(N), 3).
+format_length(millimeters(N)) :- format_length(millimeters(N), 3).
+format_length(centimeters(N)) :- format_length(centimeters(N), 3).
+format_length(meters(N))      :- format_length(meters(N), 3).
+format_length(kilometers(N))  :- format_length(kilometers(N), 3).
--- length_tests.pl
+++ length_tests.pl
@@ -9,30 +9,30 @@ output_of(Goal, Output) :-
 
 % Test cases
 test(feet_0) :-
-    output_of(write_length(feet(6), 0), "6 ft\n").
+    output_of(format_length(feet(6), 0), "6 ft\n").
 
 test(feet_default) :-
-    output_of(write_length(feet(6)), "6.000 ft\n").
+    output_of(format_length(feet(6)), "6.000 ft\n").
 
 test(inches_negative_2) :-
-    output_of(write_length(inches(-4.375), 2), "-4.38 in\n").
+    output_of(format_length(inches(-4.375), 2), "-4.38 in\n").
 
 test(inches_default) :-
-    output_of(write_length(inches(-6.875)), "-6.875 in\n").
+    output_of(format_length(inches(-6.875)), "-6.875 in\n").
 
 test(mm_1digit) :-
-    output_of(write_length(millimeters(42.42), 1), "42.4 mm\n").
+    output_of(format_length(millimeters(42.42), 1), "42.4 mm\n").
 
 test(cm_2digits) :-
-    output_of(write_length(centimeters(42.42), 2), "42.42 cm\n").
+    output_of(format_length(centimeters(42.42), 2), "42.42 cm\n").
 
 test(meters_5digits) :-
-    output_of(write_length(meters(1.2345), 5), "1.23450 m\n").
+    output_of(format_length(meters(1.2345), 5), "1.23450 m\n").
 
 test(km_6digits) :-
-    output_of(write_length(kilometers(0.75), 6), "0.750000 km\n").
+    output_of(format_length(kilometers(0.75), 6), "0.750000 km\n").
 
 test(miles_2digits) :-
-    output_of(write_length(miles(1.5), 2), "1.50 mi\n").
+    output_of(format_length(miles(1.5), 2), "1.50 mi\n").
 
 :- end_tests(length).
