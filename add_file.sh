0da8ae35760cec96b16e137a36751187ae2e376f
Joe Legner <joelegner@gmail.com>
2025-06-09 10:14:55 -0400

    Fixes add_file.sh script

--- add_file.sh
+++ add_file.sh
@@ -1,27 +1,27 @@
 #!/bin/zsh
 
 # Check for required argument
-if [[ -z '$1' ]]; then
-  echo 'Usage: $0 <filename (without extension)>'
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <filename (without extension)>"
   exit 1
 fi
 
-FILENAME='$1'
-PLFILE='${FILENAME}.pl'
-MAKEFILE='Makefile'
+FILENAME="$1"
+PLFILE="${FILENAME}.pl"
+MAKEFILE="Makefile"
 
-# Create the .pl file if it doesn't already exist
-if [[ -e '$PLFILE' ]]; then
-  echo 'File $PLFILE already exists.'
+# Create the .pl file if it doesn"t already exist
+if [[ -e "$PLFILE" ]]; then
+  echo "File $PLFILE already exists."
 else
-  touch '$PLFILE'
-  echo 'Created $PLFILE'
+  touch "$PLFILE"
+  echo "Created $PLFILE"
 fi
 
 # Append to Makefile
-echo '' >> '$MAKEFILE'
-echo '.PHONY: $FILENAME' >> '$MAKEFILE'
-echo '$FILENAME:' >> '$MAKEFILE'
-echo '\tswipl -s $PLFILE' >> '$MAKEFILE'
+echo "" >> "$MAKEFILE"
+echo ".PHONY: $FILENAME" >> "$MAKEFILE"
+echo "$FILENAME:" >> "$MAKEFILE"
+echo "\tswipl -s $PLFILE" >> "$MAKEFILE"
 
-echo 'Added $FILENAME target to $MAKEFILE'
+echo "Added $FILENAME target to $MAKEFILE"
