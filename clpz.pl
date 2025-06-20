34f77b77f063342ab4561c39c37dd9bcbc1f910a
Joe Legner <joelegner@gmail.com>
2025-06-20 19:55:58 -0400

    Adds some commentary

--- clpz.pl
+++ clpz.pl
@@ -1,5 +1,7 @@
 % clpz.pl
-% Module CLP(Z). Requires scryer-prolog.
+% Module CLP(Z). 
+
+% Note!: Requires scryer-prolog. https://www.scryer.pl/clpz
 
 :- use_module(library(clpz)).
 
@@ -30,6 +32,18 @@ scryer-prolog clpz.pl
    clpz:(N in 26..100).
    
 We get 26 songs. 26*7 = 182, and 182 > 180. Good!
+
+It is somehow important to give the domain here. Otherwise we get 25 as a result. When the predicate did not have the domain constraint in/2, this is what happens:
+
+songcount(TotalMin, MinPerSong, N) :-
+    MinPerSong*N #>= TotalMin.
+
+When we run this, we get 25, not 26. 
+
+?- songcount(180, 7, N).
+   clpz:(N in 25..sup), clpz:(180#=<7*N).
+
+Since 25*7 = 175 < 180, it is not a valid solution. This is troubling. The way to make sure we get the right answer is to declare the domain. 
 */
 
 positive_integer(N) :- 
