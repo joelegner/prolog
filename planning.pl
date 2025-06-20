65f1e04f89614909a0cb9abbcdd1af8cbee60dde
Joe Legner <joelegner@gmail.com>
2025-06-20 01:07:11 -0400

    Adds better comments to planning.pl

--- planning.pl
+++ planning.pl
@@ -1,6 +1,16 @@
 % planning.pl
-% This appears to be "pure" prolog to me. I think this is a good example of using
-% findall which seems to be a currently favored predicate in the "pure" prolog 
+
+/*
+What I'm going for here is the simplest planning scenario I could think of. It works. So the next thing would be to expand the system. Every complex system that works evolved from a simpler system that worked. 
+
+As an added benefit, this appears to be "pure" prolog. I think this is a good example of using `findall` which seems to be a currently favored predicate among the "pure" prolog proponents.
+
+The situation is this:
+
+There is an integer number line. In the starting state, our marker is at the origin, zero. Each turn or "step" we can move the marker one to the right on the number line. 
+
+There are only values from the origin to the goal. After that there are no solutions. This is the better way to think about it. Do not think about it like a loop or even an if-then statement. Just say that the relationship only holds between the origin and the goal, inclusive. 
+*/
 
 % One step: move from X to X+1, record time step.
 step(N, x(_, X0), x(N, X1)) :-
@@ -22,3 +32,22 @@ main :-
     run_findall(GoalValue, Steps),
     writeln('Steps and States:'),
     forall(member(S, Steps), writeln(S)).
+
+/*
+Example run:
+
+?- main.
+Steps and States:
+x(0,0)
+x(1,1)
+x(2,2)
+x(3,3)
+x(4,4)
+x(5,5)
+x(6,6)
+x(7,7)
+x(8,8)
+x(9,9)
+x(10,10)
+true.
+*/
