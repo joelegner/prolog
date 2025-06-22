277aa0c935438aec5f7c9229435b9a62407db330
Joe Legner <joelegner@gmail.com>
2025-06-22 12:15:22 -0400

    Implements phrase_to_file

--- phrase_to_file.pl
+++ phrase_to_file.pl
@@ -1,14 +1,22 @@
 %% phrase_to_file.pl
+:- module(phrase_to_file, [
+    phrase_to_file/2,
+    main/0,
+    file//0
+]).
 
 :- use_module(phrase_string).
 
 file --> 
-    "Hello, World".
+    "Hello, World!".
 
 main :-
-    phrase_string(file, String),        % pure
+    phrase_to_file(file, 'phrase_to_file.txt').
+
+phrase_to_file(Grammar, File) :-
+    phrase_string(Grammar, String),        % pure
     setup_call_cleanup(
-        open('phrase_to_file.txt', write, Stream),
+        open(File, write, Stream),
         write(Stream, String),          % minimal side effect here
         close(Stream)
     ).
