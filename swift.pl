1e90fa384d7146424619d60d9467975356c91c13
Joe Legner <joelegner@gmail.com>
2025-06-22 09:00:56 -0400

    Gets argument to work

--- swift.pl
+++ swift.pl
@@ -2,15 +2,16 @@
 %% swift.pl
 
 % DCG rules to generate a simple Swift main function with a loop
-swift_program -->
+swift_program(ArgNum) -->
     "func main() {", nl,
-    indent(for_loop), nl,
+    indent(for_loop(ArgNum)), nl,
     "}", nl,
     nl,
     "main()".
 
-for_loop -->
-    "for i in 1...100 {", nl,
+for_loop(ArgNum) -->
+    { number_codes(ArgNum, Codes) },
+    "for i in 1...", Codes, " {", nl,
     indent(print_line), nl,
     "    }".
 
@@ -24,7 +25,13 @@ indent(Rule) --> "    ", Rule.
 :- initialization(main, main).
 
 % Entry point predicate
-main(_) :-
-    phrase(swift_program, Codes),
+main([Arg]) :-
+    atom_number(Arg, ArgNum),
+    must_be(positive_integer, ArgNum),
+    phrase(swift_program(ArgNum), Codes),
     string_codes(String, Codes),
     writeln(String).
+
+% Entry point predicate
+main([]) :-
+    main(['100']).
