aa78281fdf663da585ee7bbdd9e999b48790034b
Joe Legner <joelegner@gmail.com>
2025-06-22 21:47:08 -0400

    Makes last_but_one elegant

--- last_but_one.pl
+++ last_but_one.pl
@@ -4,7 +4,16 @@ Find the second to last.
 */
 
 last_but_one(List, El) :-
-    writeln(List),
-    reverse(List, Reversed),
-    Reversed = [_|Rest],
-    [El|_] = Rest.
+    reverse(List, [_|[El|_]]).
+
+/* 
+This is a fantastic solution. Look at that. The second term does the work in the reverse call. It discards the first character of the reversed list. That's important to note. If you think it is removing the first element of List, that is only half of what it is doing. First it is reversing List, then lopping off the head which is the very last character.
+
+?- last_but_one([a,b,c,d,e], El).
+El = d.
+
+?- last_but_one([a,b,c,d], El).
+El = c.
+
+It works.
+*/
