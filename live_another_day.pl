42139d68a842dd0cc14a6bbcbab6b800bfd6c5af
Joe Legner <joelegner@gmail.com>
2025-06-24 11:11:17 -0400

    Improves predicate names

--- live_another_day.pl
+++ live_another_day.pl
@@ -9,18 +9,18 @@ live_another_day(
 ).
 
 minutes(live_another_day, 1440).
-weight(be_awake, 3).
-weight(sleep, 1).
-weight(rest, 1).
-weight(play, 1).
-weight(work, 3).
+activity_weight(be_awake, 3).
+activity_weight(sleep, 1).
+activity_weight(rest, 1).
+activity_weight(play, 1).
+activity_weight(work, 3).
 
-get_weight(Node, Weight) :-
-    weight(Node, Weight), !.
-get_weight(_, 1).
+node_activity_weight(Node, Weight) :-
+    activity_weight(Node, Weight), !.
+node_activity_weight(_, 1).
 
 distribute_minutes(Minutes, Nodes, Result) :-
-    maplist(get_weight, Nodes, Weights),
+    maplist(node_activity_weight, Nodes, Weights),
     sum_list(Weights, TotalWeight),
     maplist(divvy(Minutes, TotalWeight), Nodes, Weights, Result).
 
@@ -31,34 +31,34 @@ report(Report) :-
     minutes(live_another_day, Mins),
     distribute_minutes(Mins, [be_awake, sleep], Report).
 
-get_minutes(Node, Minutes) :-
+node_minutes(Node, Minutes) :-
     minutes(Node, Minutes), !.   % directly assigned
-get_minutes(Node, Minutes) :-
+node_minutes(Node, Minutes) :-
     parent_minutes(Node, ParentMinutes),
-    sibling_weights(Node, TotalWeight, NodeWeight),
+    sibling_activity_weights(Node, TotalWeight, NodeWeight),
     Minutes is ParentMinutes * NodeWeight / TotalWeight.
 
-children(live_another_day, [be_awake, sleep]).
-children(be_awake, [rest, play, work]).
-children(sleep, []).
-children(rest, []).
-children(play, []).
-children(work, []).
+activity_children(live_another_day, [be_awake, sleep]).
+activity_children(be_awake, [rest, play, work]).
+activity_children(sleep, []).
+activity_children(rest, []).
+activity_children(play, []).
+activity_children(work, []).
 
 parent(Child, Parent) :-
-    children(Parent, Children),
+    activity_children(Parent, Children),
     member(Child, Children).
 
 parent_minutes(Child, ParentMinutes) :-
     parent(Child, Parent),
     evaluate(Parent, ParentMinutes).
 
-sibling_weights(Node, TotalWeight, NodeWeight) :-
+sibling_activity_weights(Node, TotalWeight, NodeWeight) :-
     parent(Node, Parent),
-    children(Parent, Siblings),
-    maplist(get_weight, Siblings, Weights),
+    activity_children(Parent, Siblings),
+    maplist(node_activity_weight, Siblings, Weights),
     sum_list(Weights, TotalWeight),
-    get_weight(Node, NodeWeight).
+    node_activity_weight(Node, NodeWeight).
 
 evaluate(Node, Minutes) :-
-    get_minutes(Node, Minutes).
+    node_minutes(Node, Minutes).
