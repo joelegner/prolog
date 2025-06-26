88ea5ab828fd8b519241ee5601f12b4fe1d33acc
Joe Legner <joelegner@gmail.com>
2025-06-25 23:35:33 -0400

    Adds commentary

--- init.pl
+++ init.pl
@@ -10,6 +10,19 @@ chars
 string
 */
 
+/*
+Reasoning for using clpfd module.
+
+https://www.swi-prolog.org/pldoc/man?section=clpfd-integer-arith
+
+"The arithmetic constraints (section A.9.2) #=/2, #>/2 etc. are meant to be used instead of the primitives (is)/2, (=:=)/2, (>)/2 etc. over integers. Almost all Prolog programs also reason about integers. Therefore, it is recommended that you put the following directive in your <config>/init.pl initialisation file to make CLP(FD) constraints available in all your programs:
+
+:- use_module(library(clpfd)).
+
+Throughout the following, it is assumed that you have done this."
+
+The only other way to do this would be to put :- use_module(library(clpfd) in separate modules. Instead of this, I went ahead and added the following:
+*/
 :- use_module(library(clpfd)).
 
 % Clear the screen
