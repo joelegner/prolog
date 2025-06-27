13eb8e14021529f4c562529aa9700aaf1d04f1c2
Joe Legner <joelegner@gmail.com>
2025-06-27 16:53:20 -0400

    Adds atom_phonetic_list

--- nato_alphabet.pl
+++ nato_alphabet.pl
@@ -30,7 +30,26 @@ letter_phonetic(z, zulu).
 nato_alphabet_pairs(NAP) :-
     findall(L-P, letter_phonetic(L, P), NAP).
 
+atom_phonetic_list(Atom, Phonetics) :-
+    atom_chars(Atom, Chars),
+    maplist(downcase_char, Chars, LowerChars),
+    atom_phonetic_list_(LowerChars, Phonetics).
+
+atom_phonetic_list_([], []).
+atom_phonetic_list_([A|As], [P|Ps]) :-
+    atom_char(A),
+    letter_phonetic(A, P),
+    atom_phonetic_list_(As, Ps).
+
+downcase_char(Char, Lower) :-
+    char_type(Char, upper),
+    char_type(Lower, to_lower(Char)).
+downcase_char(Char, Char).
+
+atom_char(C) :- char_type(C, alpha).
+
 run :-
     nato_alphabet_pairs(NAP),
-    % This is a cool way to print all of a list
-    maplist(writeln, NAP).
+    maplist(writeln, NAP),
+    atom_phonetic_list(github, Ps),
+    writeln(Ps).
