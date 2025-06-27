76f993d36570cbae1f620a5783a15cd2fa54dca7
Joe Legner <joelegner@gmail.com>
2025-06-27 17:00:39 -0400

    Adds tests for NATO alphabet

--- nato_alphabet.pl
+++ nato_alphabet.pl
@@ -1,31 +1,29 @@
 %% nato_alphabet.pl
 
-letter_phonetic(a, alpha).
-letter_phonetic(b, beta).
-letter_phonetic(c, charlie).
-letter_phonetic(d, delta).
-letter_phonetic(e, echo).
-letter_phonetic(f, foxtrot).
-letter_phonetic(g, golf).
-letter_phonetic(h, hotel).
-letter_phonetic(i, india).
-letter_phonetic(j, juliet).
-letter_phonetic(k, kilo).
-letter_phonetic(l, lima).
-letter_phonetic(m, mike).
-letter_phonetic(n, november).
-letter_phonetic(o, oscar).
-letter_phonetic(p, papa).
-letter_phonetic(q, quebec).
-letter_phonetic(r, romeo).
-letter_phonetic(s, sierra).
-letter_phonetic(t, tango).
-letter_phonetic(u, uniform).
-letter_phonetic(v, victor).
-letter_phonetic(w, whisky).
-letter_phonetic(x, xray).
-letter_phonetic(y, yankee).
-letter_phonetic(z, zulu).
+% Here we start with our tests and then satisfy them below. It's an experiment.
+:- begin_tests(test_nato).
+
+test(test_letter_count) :-
+    nato_alphabet_pairs(NAP),
+    length(NAP, Len),
+    Len =:= 26. 
+
+:- end_tests(test_nato).
+
+% Relate all 26 English letters with their phonetic versions.
+letter_phonetic(a, alpha).      letter_phonetic(b, beta).
+letter_phonetic(c, charlie).    letter_phonetic(d, delta).
+letter_phonetic(e, echo).       letter_phonetic(f, foxtrot).
+letter_phonetic(g, golf).       letter_phonetic(h, hotel).
+letter_phonetic(i, india).      letter_phonetic(j, juliet).
+letter_phonetic(k, kilo).       letter_phonetic(l, lima).
+letter_phonetic(m, mike).       letter_phonetic(n, november). 
+letter_phonetic(o, oscar).      letter_phonetic(p, papa).
+letter_phonetic(q, quebec).     letter_phonetic(r, romeo).
+letter_phonetic(s, sierra).     letter_phonetic(t, tango). 
+letter_phonetic(u, uniform).    letter_phonetic(v, victor).
+letter_phonetic(w, whisky).     letter_phonetic(x, xray).
+letter_phonetic(y, yankee).     letter_phonetic(z, zulu).
 
 nato_alphabet_pairs(NAP) :-
     findall(L-P, letter_phonetic(L, P), NAP).
