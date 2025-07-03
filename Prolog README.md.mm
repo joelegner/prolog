49b3052b5e580c41cf23cfdee9490d87567e013c
Joe Legner <joelegner@gmail.com>
2025-07-02 23:39:32 -0400

    Adds FreePlane mind map for docs

---
+++ Prolog README.md.mm
@@ -0,0 +1,122 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Prolog README.md" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_452131666" CREATED="1610381621610" MODIFIED="1751485767492">
+<font NAME="Times New Roman"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/joes-freeplane-template.mm" show_note_icons="true" followedTemplateLocation="template:/joes-freeplane-template.mm" followedMapLastTime="1656438878472" fit_to_viewport="false" show_icons="BESIDE_NODES" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_207122690" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_207122690" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" ALPHA="230" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_3752836">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_3752836"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Introduction to Prolog" POSITION="bottom_or_right" ID="ID_573476176" CREATED="1751486846782" MODIFIED="1751486930958">
+<node TEXT="Notes from learning" ID="ID_232822087" CREATED="1751486852886" MODIFIED="1751486857150"/>
+<node TEXT="Currated list of" ID="ID_1137882168" CREATED="1751486871758" MODIFIED="1751486881062">
+<node TEXT="Predicates" ID="ID_315636863" CREATED="1751486881205" MODIFIED="1751486893702"/>
+<node TEXT="Modules" ID="ID_531871066" CREATED="1751486882909" MODIFIED="1751486891182"/>
+</node>
+<node TEXT="Replace sections when better things are learned" ID="ID_1830895022" CREATED="1751486857693" MODIFIED="1751486870534"/>
+</node>
+<node TEXT="Links to Prolog resources" POSITION="bottom_or_right" ID="ID_185819872" CREATED="1751486908117" MODIFIED="1751486928646">
+<node TEXT="Power of Prolog" ID="ID_1942344959" CREATED="1751486995012" MODIFIED="1751486997437"/>
+<node TEXT="SWI Prolog" ID="ID_40514490" CREATED="1751486998988" MODIFIED="1751487000669">
+<node TEXT="http://www.swi-prolog.org/" ID="ID_7419063" CREATED="1751487024705" MODIFIED="1751487024705" LINK="http://www.swi-prolog.org/"/>
+</node>
+<node TEXT="Scryer Prolog" ID="ID_1115156223" CREATED="1751487000836" MODIFIED="1751487003861"/>
+</node>
+<node TEXT="Notable Prolog people" POSITION="bottom_or_right" ID="ID_1515639540" CREATED="1751486919669" MODIFIED="1751486924654"/>
+<node TEXT="Glossary of Prolog terminology" POSITION="bottom_or_right" ID="ID_1058936135" CREATED="1751485830835" MODIFIED="1751486940453"/>
+<node TEXT="Lists" POSITION="bottom_or_right" ID="ID_1369733757" CREATED="1751485769771" MODIFIED="1751485771556">
+<node TEXT="List syntax" ID="ID_1437870552" CREATED="1751486234901" MODIFIED="1751486237477">
+<node TEXT="[]" ID="ID_1944498574" CREATED="1751486760623" MODIFIED="1751486762159"/>
+<node TEXT="[a,b,c]" ID="ID_312264337" CREATED="1751486241837" MODIFIED="1751486269373"/>
+<node TEXT="[a,b,c|T]" ID="ID_322790720" CREATED="1751486241837" MODIFIED="1751486266709"/>
+<node TEXT="[H|T]" ID="ID_1786455177" CREATED="1751486246805" MODIFIED="1751486254925"/>
+</node>
+<node TEXT="List predicates" ID="ID_1965208296" CREATED="1751486725959" MODIFIED="1751486728855">
+<node TEXT="append/3" ID="ID_55789357" CREATED="1751486213757" MODIFIED="1751486717647"/>
+<node TEXT="list_to_set" ID="ID_344720175" CREATED="1751486804966" MODIFIED="1751486807176"/>
+<node TEXT="reverse/3" ID="ID_1078966252" CREATED="1751486297453" MODIFIED="1751486714992"/>
+<node TEXT="select/3" ID="ID_1051524229" CREATED="1751486736568" MODIFIED="1751486738615"/>
+</node>
+<node TEXT="List actions" ID="ID_1651748531" CREATED="1751486743079" MODIFIED="1751486745376">
+<node TEXT="Append" ID="ID_842123134" CREATED="1751486745687" MODIFIED="1751486747479"/>
+<node TEXT="Concatenate" ID="ID_1994918084" CREATED="1751486748239" MODIFIED="1751486749752"/>
+<node TEXT="Insert" ID="ID_1343446643" CREATED="1751486751679" MODIFIED="1751486752775"/>
+<node TEXT="Remove" ID="ID_120561101" CREATED="1751486749919" MODIFIED="1751486751520"/>
+</node>
+</node>
+<node TEXT="Aggregate" POSITION="bottom_or_right" ID="ID_1181482889" CREATED="1751486199549" MODIFIED="1751486201700"/>
+<node TEXT="Sets" POSITION="bottom_or_right" ID="ID_1820285800" CREATED="1751485788331" MODIFIED="1751485789436"/>
+<node TEXT="Recursion" POSITION="bottom_or_right" ID="ID_1125648743" CREATED="1751485772659" MODIFIED="1751485776572"/>
+<node TEXT="DCG" POSITION="bottom_or_right" ID="ID_1215041536" CREATED="1751485817195" MODIFIED="1751485820884"/>
+<node TEXT="CLP" POSITION="bottom_or_right" ID="ID_835027974" CREATED="1751485821067" MODIFIED="1751485822924"/>
+<node TEXT="Introspection" POSITION="bottom_or_right" ID="ID_1301191642" CREATED="1751486698295" MODIFIED="1751486703464">
+<node TEXT="listing/1" ID="ID_246018786" CREATED="1751486705448" MODIFIED="1751486706912"/>
+</node>
+</node>
+</map>
