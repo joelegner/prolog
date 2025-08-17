36e79a9bae54eed1ee729c50315986fea24f60c7
Joe Legner <joelegner@gmail.com>
2025-08-17 17:03:14 -0400

    Adds ottawa.pl

---
+++ ottawa.pl
@@ -0,0 +1,25 @@
+% Ottawa house 
+room(3, dining_room).
+room(2, living_room).
+room(6, hallway).
+room(7, master_bedroom).
+room(8, back_bedroom).
+room(4, kitchen).
+room(1, porch).
+room(5, carnival_room).
+room(9, basement).
+room(10, coal_room). 
+room(11, upstairs_bedroom).
+room(12, bathroom).
+connected(1, 2).
+connected(2, 3).
+connected(3, 4).
+connected(4, 5).
+connected(4, 9).
+connected(9, 10).
+connected(2, 6).
+connected(6, 12).
+connected(6, 7).
+connected(6, 8).
+connected(4, 11).
+
