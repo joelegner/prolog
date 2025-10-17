0a3ede1e747f16d11ae3d8c6105a7f738a9e27ce
Joe Legner <joelegner@gmail.com>
2025-10-17 19:42:21 -0400

    Adds print_report/0

--- twitch.pl
+++ twitch.pl
@@ -16,16 +16,28 @@ project_production_system(p9, dragon).
 
 cn(cn1, 'Make $1 streaming Angband on Twitch').
 
-% Top Level
+% Top Level FR
 fr_name_cns(fr0, 'Make first $1 streaming Angband on Twitch', [cn1]).
+
+% Design Parameters (DP)
 dp_name(dp0, Name) :- production_system_acronym(dragon, Name).
+dp_name(dp1, 'DRAGON Design System').
+dp_name(dp2, 'DRAGON Business System').
+dp_name(dp3, 'DRAGON Creative System').
 
 % Start zig-zagging
 fr_zig_dps(fr0, [dp0]).
+fr_zig_dps(fr1, [dp1]).
+fr_zig_dps(fr2, [dp2]).
+fr_zig_dps(fr3, [dp3]).
 
 parent_fr_child_fr_name(f0, fr1, 'Design the DRAGON system').
 parent_fr_child_fr_name(f0, fr2, 'Run the business side').
 parent_fr_child_fr_name(f0, fr3, 'Produce Twitch streams').
+
+fr_dp(FR, DP) :-
+    fr_zig_dps(FR, DPs),
+    member(DP, DPs).
 
 fr(FR) :- 
     fr_name_cns(FR, _, _).
@@ -37,15 +49,29 @@ fr_name(FR, Name) :-
 fr_name(FR, Name) :-
     parent_fr_child_fr_name(_, FR, Name).
 
-fr_upper(FR, FRUpper) :-
-    atom_string(FR, FRStr),               % Convert atom to string
-    string_upper(FRStr, FRUpper).
+atom_upper(Atom, AtomUpper) :-
+    atom_string(Atom, AtomString),               % Convert atom to string
+    string_upper(AtomString, AtomUpper).
+
+format_dp(DP) :- 
+    atom_upper(DP, DPUpper),
+    dp_name(DP, DPName),
+    format('~w: ~w~n', [DPUpper, DPName]).
 
 format_fr(FR) :- 
+    atom_upper(FR, FRUpper),
     fr_name(FR, FRName),
-    fr_upper(FR, FRUpper),
     format('~w: ~w~n', [FRUpper, FRName]).
 
 frs(FRs) :- findall(FR, fr(FR), FRs).
 
 dp_zag_frs(dp0, [fr1, fr2, fr3]).
+
+print_fr_dp(FR, DP) :-
+    format_fr(FR),
+    format_dp(DP),
+    format('~n').
+
+print_report :-
+    format('FR-DP Pairs:~n~n'),
+    forall(fr_dp(FR, DP), print_fr_dp(FR, DP)).
